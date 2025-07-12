e0957f74be035a2d6415179c33c4d69d377dcd7a
TaiwoDaniel <taiwodaniel3116@gmail.com>
2025-07-12 13:24:04 -0700

    Added scroll  animation

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ backdrop.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('active');
   backdrop.classList.toggle('active');
 })
+
+// ANIMATE ON SCROLL //
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+   console.log(entry);
+   if (entry.isIntersecting) {
+     entry.target.classList.add('show');
+   } else {
+     entry.target.classList.remove('show');
+   }
+  })
+})
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
