02795f5922f1b7d9537aa6716f2c3be6946936fa
TaiwoDaniel <taiwodaniel3116@gmail.com>
2025-07-29 10:08:43 -0700

    Added more animations

--- index.js
+++ index.js
@@ -26,8 +26,32 @@ const observer = new IntersectionObserver((entries) => {
    }
   })
 })
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+   console.log(entry);
+   if (entry.isIntersecting) {
+     entry.target.classList.add('show-2');
+   } else {
+     entry.target.classList.remove('show-2');
+   }
+  })
+})
+const observer3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+   console.log(entry);
+   if (entry.isIntersecting) {
+     entry.target.classList.add('show-3');
+   } else {
+     entry.target.classList.remove('show-3');
+   }
+  })
+})
 const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements2 = document.querySelectorAll('.hidden-2');
+const hiddenElements3 = document.querySelectorAll('.hidden-3');
 hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements2.forEach((el) => observer2.observe(el));
+hiddenElements3.forEach((el) => observer3.observe(el));
 
 // 3D SPHERE ANIMATION //
 const sphere = document.getElementById('sphere');
